ab0241296ec75abfe99bd9903d1fa55988866373
Adrian N. Riadi <adrianovriadi@gmail.com>
2026-04-19 16:43:18 +0700

    Validate user ID

--- src/modules/user/router.ts
+++ src/modules/user/router.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { UserController } from "./controller.js";
-import { validate, updateSchema, registerSchema } from "./validator.js";
+import { idParamValidation, validate, updateSchema, registerSchema } from "./validator.js";
 
 export const userRouter = (controller: UserController): Router => {
     const router = Router();
 
     router.post('/register', validate(registerSchema), controller.register);
-    router.get('/:id', controller.getById);
-    router.put('/:id', validate(updateSchema), controller.update);
-    router.patch('/deactivate/:id/', controller.deactivate);
-    router.delete('/:id', controller.delete);
+    router.get('/:id', idParamValidation, controller.getById);
+    router.put('/:id', idParamValidation, validate(updateSchema), controller.update);
+    router.patch('/deactivate/:id/', idParamValidation, controller.deactivate);
+    router.delete('/:id', idParamValidation, controller.delete);
 
     return router;
 }
--- src/modules/user/validator.ts
+++ src/modules/user/validator.ts
@@ -1,11 +1,24 @@
 import Joi from "joi";
 import { Request, Response, NextFunction } from "express";
 import { RegisterUserDTO, UpdateUserDTO }  from "./model.js";
+import { config } from "../../config/index.js";
 
 const validTimezones = Intl.supportedValuesOf("timeZone");
 const invalidTimezoneMsg = {
     "any.only": "Timezone value is not a valid IANA timezone"
 };
+
+export const createIdSchema = () => {
+  if (config.dbType === 'mongodb') {
+    return Joi.string().hex().length(24).required();
+  }
+  // Add other DB types here as needed
+  return Joi.string().required();
+}
+
+export const idParamSchema = Joi.object({
+  id: createIdSchema(),
+});
 
 export const registerSchema = Joi.object<RegisterUserDTO>({
   name: Joi.string().required(),
@@ -27,6 +40,15 @@ export const updateSchema = Joi.object<UpdateUserDTO>({
   birthday: Joi.date(),
   active: Joi.boolean(),
 }).min(1).unknown(false); // At least one field must be provided, and no unknown fields allowed
+
+export const idParamValidation = (req: Request, res: Response, next: NextFunction): void => {
+  const { error } = idParamSchema.validate(req.params);
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+    return;
+  }
+  next();
+};
 
 export const validate = (schema: Joi.ObjectSchema) =>
     (req: Request, res: Response, next: NextFunction): void => {
