cc00afa7c734dbb344c78c2bfe1073988a20cdf3
Adrian N. Riadi <adrianovriadi@gmail.com>
2026-04-19 15:25:42 +0700

    User controller and router
    Implementation of controller-level and router-level user CRUD module

---
+++ src/modules/user/router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { UserController } from "./controller.js";
+
+export const userRouter = (controller: UserController): Router => {
+    const router = Router();
+
+    router.post('/register', controller.register);
+    router.get('/:id', controller.getById);
+    router.put('/:id', controller.update);
+    router.patch('/:id/deactivate', controller.deactivate);
+    router.delete('/:id', controller.delete);
+
+    return router;
+}
