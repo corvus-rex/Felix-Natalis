7aa06c329e885c18a15fb41d36c26c99acabb6ce
Adrian N. Riadi <adrianovriadi@gmail.com>
2026-04-19 15:50:55 +0700

    Fix router name

--- src/modules/user/router.ts
+++ src/modules/user/router.ts
@@ -7,7 +7,7 @@ export const userRouter = (controller: UserController): Router => {
     router.post('/register', controller.register);
     router.get('/:id', controller.getById);
     router.put('/:id', controller.update);
-    router.patch('/:id/deactivate', controller.deactivate);
+    router.patch('/deactivate/:id/', controller.deactivate);
     router.delete('/:id', controller.delete);
 
     return router;
